3f55951908a0af65e96df84f060261105ce2cbf8
lwinmoehein <dev.lwinmoehein@gmail.com>
2019-11-12 09:26:45 -0800

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,73 @@
 <template>
+
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header/>
+    <Todos v-bind:todos="todos" v-on:del-todo="deleteToDo"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Todos from './components/ToDo'
+import Header from './components/layouts/header'
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Todos,
+    Header
+  },
+  data(){
+    return {
+      todos:[
+        {
+          id:1,
+          title:"todo one",
+          completed:true,
+        },
+        {
+          id:2,
+          title:"todo two",
+          completed:false,
+        },
+        { 
+          id:3,
+          title:"todo three",
+          completed:false,
+        },
+        {
+          id:4,
+          title:"todo four",
+          completed:true,
+        },
+      ]
+    }
+  },
+  methods:{
+    deleteToDo(id){
+      this.todos=this.todos.filter(todo=>todo.id!==id);
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+ * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+  body {
+    font-family: Arial, Helvetica, sans-serif;
+    line-height: 1.4;
+  }
+  .btn {
+    display: inline-block;
+    border: none;
+    background: #555;
+    color: #fff;
+    padding: 7px 20px;
+    cursor: pointer;
+  }
+  .btn:hover {
+    background: #666;
+  }
 </style>
---
+++ src/components/ToDo.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <div v-for="todo in todos" v-bind:key="todo.key">
+            <ToDoItem v-bind:todo="todo" v-on:del-todo="$emit('del-todo',todo.id)"/>
+        </div>
+    </div>
+</template>
+<script>
+import ToDoItem from "./ToDoItem.vue"
+export default {
+    name:"Todos",
+    props:["todos"],
+    components:{
+        ToDoItem
+    },
+}
+</script>
+<style scoped>
+
+</style>
